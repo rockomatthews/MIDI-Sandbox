954ecc1f095476425f95c919ad02a7e65076adc1
Rob Matthews <rob@robmatthe.ws>
2023-05-19 07:35:10 +0000

    Created dropdown for MIDI devices that are connected. Once selected, instrument plays now using tone.js provided synth. Also buttons in place to create the logic for recording, playing, clearing a recording, and saving which I'll use tone.js to do as much of as possible

--- src/components/MIDIComponent.jsx
+++ src/components/MIDIComponent.jsx
@@ -1,18 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as Tone from 'tone';
-import { Midi } from 'tone'; // corrected line
+import { Midi } from 'tone';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
 
 const MIDIComponent = () => {
+  const AudioContext = window.AudioContext || window.AudioContext;
 
+  const audioContext = new AudioContext();
   const synth = new Tone.Synth().toDestination();
   const [midiAccess, setMidiAccess] = useState(null);
   const [open, setOpen] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
+  const [selectedDevice, setSelectedDevice] = useState('');
 
-  const handleOpen = () => setOpen(true);
+  const handleOpen = async () => {
+    await Tone.start();
+    console.log('audio is ready');
+    setOpen(true);
+  };
+
   const handleClose = () => {
     setOpen(false);
     synth.triggerRelease();
@@ -23,8 +35,24 @@ const MIDIComponent = () => {
     // Add your recording logic here
   };
 
+  const saveRecording = () => {
+    // Add your saving logic here
+  };
+
+  const clearRecording = () => {
+    // Add your clear logic here
+  };
+
+  const playbackRecording = () => {
+    // Add your playback logic here
+  };
+
+  const handleChange = (event) => {
+    setSelectedDevice(event.target.value);
+  };
+
   useEffect(() => {
-    if(navigator.requestMIDIAccess){
+    if (navigator.requestMIDIAccess) {
       navigator.requestMIDIAccess().then(
         (midiAccess) => {
           setMidiAccess(midiAccess);
@@ -58,28 +86,45 @@ const MIDIComponent = () => {
     };
 
     return () => synth.dispose();
-
-  }, []); 
+  }, []);
 
   return (
     <div>
       <h1>Web MIDI API Component</h1>
-      {midiAccess && Array.from(midiAccess.inputs.values()).map((input, index) => (
-        <Button variant="contained" color="primary" onClick={handleOpen} key={index}>
-          {`Use MIDI Device ${index+1}`}
-        </Button>
-      ))}
+      <Button variant="contained" color="primary" onClick={handleOpen}>
+        Select MIDI Device
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby="select-midi-device"
       >
         <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4 }}>
-          <h2 id="modal-modal-title">MIDI Controller</h2>
-          <p id="modal-modal-description">Use your MIDI device to control the synth. Press the button to start or stop recording.</p>
-          <Button variant="contained" color={isRecording ? "secondary" : "primary"} onClick={handleRecording}>
+          <FormControl fullWidth>
+            <InputLabel id="select-midi-device">Select MIDI Device</InputLabel>
+            <Select
+              labelId="select-midi-device"
+              id="select-midi-device"
+              value={selectedDevice}
+              label="Select MIDI Device"
+              onChange={handleChange}
+            >
+              {midiAccess && Array.from(midiAccess.inputs.values()).map((input, index) => (
+                <MenuItem value={index} key={index}>{input.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Button variant="contained" color={isRecording ? "secondary" : "primary"} onClick={handleRecording} sx={{ mt: 2 }}>
             {isRecording ? "Stop Recording" : "Start Recording"}
+          </Button>
+          <Button variant="contained" color="primary" onClick={saveRecording} sx={{ mt: 2 }}>
+            Save as .wav
+          </Button>
+          <Button variant="contained" color="primary" onClick={clearRecording} sx={{ mt: 2 }}>
+            Clear Recording
+          </Button>
+          <Button variant="contained" color="primary" onClick={playbackRecording} sx={{ mt: 2 }}>
+            Playback Recording
           </Button>
         </Box>
       </Modal>
